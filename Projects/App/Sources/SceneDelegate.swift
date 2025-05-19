28703e58af70e05d4c1f22d4e97338b306520df0
gomhyeok <gomhyeok@gmail.com>
2025-05-19 15:34:05 +0900

    [FEAT] - APP 모듈 Delegate 설정 - SceneDelegate + AppDelegate - 화면 관리 = SceneDelegate, 앱 생명주기 관리 모듈 = AppDelegate

---
+++ Projects/App/Sources/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  Meow
+//
+//  Created by 최재혁 on 5/19/25.
+//
+
+import UIKit
+
+class SceneDelegate : UIResponder, UIWindowSceneDelegate {
+    
+    // Scene생성시 UI를 구성
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        
+    }
+    
+    // 화면이 백그라운드로 들어가는 경우
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+}
